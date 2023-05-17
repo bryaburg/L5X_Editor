708c2fde3e7fbb6e9e81fb41ed5713eb6dcb5465
Bryan Burgess <bryanburgess95@hotmail.com>
2023-05-17 13:37:29 -0500

    Edit Rung 0 txt

--- MES Core Build/Core_Build.py
+++ MES Core Build/Core_Build.py
@@ -1,9 +1,18 @@
 import xml.etree.ElementTree as ET
 
-xmlfile = "_002_IGNITION_DATA_OVERALL_MACHINE.L5X"
+xmlfile = (r"C:\Users\burgeB9\Desktop\L5X_Editor\MES Core Build\_002_IGNITION_DATA_OVERALL_MACHINE.L5X")
 tree = ET.parse(xmlfile)
-root = tree.getroot()
+RSLogix5000Content = tree.getroot()
 
-p = tree.find("./Controller/Programs/Program/Routines/Routine/RLLContent/Rung/Text")
-#p.text = 'XIO(LNXX_SCHEDULED_RUN)MOV(1,LNXX_TO_DC01_DOWNTIME_CODE[0])'
-print(p.text)
+#ET.dump(tree)
+
+# Select Rung 0
+Rung0 = RSLogix5000Content.find(".//Rung[@Number= '0']/Text")
+print(Rung0.tag, Rung0.attrib, Rung0.text)
+
+# Change Value in Rung 0 
+
+for Rung in RSLogix5000Content.findall(".//Rung[@Number= '0']/Text"):
+    Rung.text = "XIC(LNXX_SCHEDULED_RUN)MOV(0,LNXX_TO_DC01_DOWNTIME_CODE[0]);"
+    print(Rung.tag, Rung.attrib, Rung.text)
+    ET.SubElement(Rung, "")
